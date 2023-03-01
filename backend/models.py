bc46f61b60f146af088de7ccd0ac9478132f957f
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-03-02 00:38:54 +0530

    fix: increase textfield size

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     title = models.CharField(max_length=300)
-    text = models.CharField(max_length=400)
+    text = models.CharField(max_length=4000)
     username = models.CharField(max_length=300, default="Anonymous")
 
     def __str__(self):
