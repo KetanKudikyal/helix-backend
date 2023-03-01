6f169e4d32faae42b4e714b543f0765ec9f36524
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-03-01 22:46:08 +0530

    fix: change default value

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=300)
     text = models.CharField(max_length=400)
-    username = models.CharField(max_length=300, default="Unknown")
+    username = models.CharField(max_length=300, default="Anonymous")
 
     def __str__(self):
         return self.title + ' ' + self.text
