3083d09d571ed188ba25b29732d42552741e4f3f
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2023-03-01 22:33:44 +0530

    fix: add username

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Question(models.Model):
     title = models.CharField(max_length=300)
     text = models.CharField(max_length=400)
+    username = models.CharField(max_length=300, default="Unknown")
 
     def __str__(self):
         return self.title + ' ' + self.text
-        
--- backend/serializers.py
+++ backend/serializers.py
@@ -1,7 +1,9 @@
 from dataclasses import fields
 from rest_framework import serializers
 from .models import Question
+
+
 class QuestionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ["id" ,"title" , "text"]
+        fields = ["id", "title", "text", "username"]
